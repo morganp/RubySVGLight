76b144fd1b312e809e7aa7c38dbf15ae9189b339
Morgan Prior <morgan@amaras-tech.co.uk>
2011-05-09 22:46:09 +0100

    Added some text to the example. With setting global options and overriding global options.

--- examples/shapes.rb
+++ examples/shapes.rb
@@ -8,6 +8,12 @@ canvas = RubySVGLight::Document.new
 canvas.circle(40,40,20)
 canvas.rectangle(80,20,40,40)
 canvas.line(20,80,100,0)
-canvas.line(20,100,100,0)
+canvas.line(20,105,100,0)
+
+options = {:font_size=>'12px'}
+canvas.options(options)
+canvas.text(20,100, 'HelloWorld!')
+
+canvas.text(20,130, 'HelloWorld!', {:font_size=>'18px'})
 
 canvas.to_file('shapes.svg')
