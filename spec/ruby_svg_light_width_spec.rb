8dae798ef6c04f7a7722738e814312058741eda4
Morgan Prior <morgan@amaras-tech.co.uk>
2011-05-11 00:03:21 +0100

    Added rspec tests for auto width calculations

---
+++ spec/ruby_svg_light_width_spec.rb
@@ -0,0 +1,54 @@
+
+require File.join( '..', 'lib', 'ruby_svg_light')
+
+def check_width_height(svg, width, height)
+    svg.width.should    == width
+    svg.height.should   == height
+end
+
+describe RubySVGLight, "RubySVGLight" do
+  it "Check width calculations, objects from origin" do
+    canvas = RubySVGLight::Document.new()
+    check_width_height(canvas, 0, 0)
+
+    canvas.rectangle(0,0,10,20)
+    check_width_height(canvas, 10, 20)
+
+    canvas.line(0,0, 30, 40)
+    check_width_height(canvas, 30, 40)
+
+    canvas.circle(0,0,100)
+    check_width_height(canvas, 100, 100)
+  end
+
+  it "Smaller new component should not shrink size" do
+    canvas = RubySVGLight::Document.new()
+    canvas.circle(0,0,100)
+    check_width_height(canvas, 100, 100)
+    
+    canvas.circle(0,0,50)
+    check_width_height(canvas, 100, 100)
+
+    canvas.line(0,0, 60, 40)
+    check_width_height(canvas, 100, 100)
+
+    canvas.rectangle(0,0,10,20)
+    check_width_height(canvas, 100, 100)
+  end
+
+  it "Check width calculations, objects not on origin" do
+    canvas = RubySVGLight::Document.new()
+    check_width_height(canvas, 0, 0)
+
+    canvas.rectangle(15,25,10,20)
+    check_width_height(canvas, 25, 45)
+
+    canvas.line(35,45, 30, 40)
+    check_width_height(canvas, 65, 85)
+
+    canvas.circle(10,15,100)
+    check_width_height(canvas, 110, 115)
+  end
+end
+
+
